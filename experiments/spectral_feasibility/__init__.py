5696b415ac3caf3f54665cba5eb79668f6252733
DwightEd <96425317+DwightEd@users.noreply.github.com>
2026-08-14 17:13:29 +0800

    Add spectral feasibility experiment package

---
+++ experiments/spectral_feasibility/__init__.py
@@ -0,0 +1,5 @@
+"""Spectral node-representation feasibility experiments."""
+
+from .representations import SpectralConfig, spectral_token_representation
+
+__all__ = ["SpectralConfig", "spectral_token_representation"]
