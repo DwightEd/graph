d55d073573f1f966fbbef32983e44da1de6bcf22
DwightEd <96425317+DwightEd@users.noreply.github.com>
2026-08-19 02:43:09 +0800

    Export mechanism-guided CASF interfaces

--- experiments/causal_setflow/__init__.py
+++ experiments/causal_setflow/__init__.py
@@ -1,16 +1,30 @@
-"""Learnable causal attention Set-Flow representations."""
+"""Mechanism-Guided Causal Attention Set-Flow."""
 
-from .config import SetFlowModelConfig, SourceSetConfig, TrainingConfig
+from .config import (
+    CORRUPTION_NAMES,
+    CorruptionConfig,
+    SetFlowModelConfig,
+    SourceSetConfig,
+    TrainingConfig,
+)
+from .corruptions import CorruptionPlan, apply_corruption, sample_corruption_plan
 from .data import CausalSourceSetGraph, LayerSourceSets, extract_causal_source_set_graph
-from .model import CausalSetFlowModel, SetFlowOutput
+from .model import CausalSetFlowModel, EncoderOutput, EnergyOutput, SetFlowEncoder
 
 __all__ = [
+    "CORRUPTION_NAMES",
     "CausalSetFlowModel",
     "CausalSourceSetGraph",
+    "CorruptionConfig",
+    "CorruptionPlan",
+    "EncoderOutput",
+    "EnergyOutput",
     "LayerSourceSets",
+    "SetFlowEncoder",
     "SetFlowModelConfig",
-    "SetFlowOutput",
     "SourceSetConfig",
     "TrainingConfig",
+    "apply_corruption",
     "extract_causal_source_set_graph",
+    "sample_corruption_plan",
 ]
