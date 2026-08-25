126af5c44755673c3ff4317a5d6ed69d6131f44e
DwightEd <96425317+DwightEd@users.noreply.github.com>
2026-08-25 17:41:21 +0800

    Add public module import test

---
+++ experiments/holoroute/tests/test_imports.py
@@ -0,0 +1,7 @@
+def test_readable_refactor_modules_import():
+    import experiments.holoroute.baseline
+    import experiments.holoroute.detection
+    import experiments.holoroute.evaluate
+    import experiments.holoroute.learning
+    import experiments.holoroute.pipeline
+    import experiments.holoroute.run
