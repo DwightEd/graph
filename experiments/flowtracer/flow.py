696f8f6f42c8f4ae843fa21899fa5c1f32bdd78c
DwightEd <96425317+DwightEd@users.noreply.github.com>
2026-09-14 15:28:06 +0800

    scaffold(flowtracer): declare potential, reweighting and throughput functions

---
+++ experiments/flowtracer/flow.py
@@ -0,0 +1,18 @@
+def compute_reachability_potential():
+    pass
+
+
+def reweight_edges():
+    pass
+
+
+def propagate_flow():
+    pass
+
+
+def compute_edge_flow():
+    pass
+
+
+def compute_node_throughput():
+    pass
